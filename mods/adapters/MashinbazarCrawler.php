86b70a3f079915c8442517d1c46b2e69b5f0b8a0
Sepehr Lajevardi <me@sepehr.ws>
2014-02-01 22:55:28 +0330

    Added a new sample adapter.

--- mods/adapters/MashinbazarCrawler.php
+++ mods/adapters/MashinbazarCrawler.php
@@ -1,5 +1,8 @@
 <?php defined('PATH') OR exit('No direct script access allowed');
 
+// Include the PhoneExtractorCrawler
+require_once PATH . 'mods/PhoneExtractorCrawler.php';
+
 /**
  * Crawler adapter class for new mashinbazar.com
  *
@@ -15,7 +18,7 @@
  * @copyright	Copyright (c) Sepehr Lajevardi
  * @license		WTFPL - http://www.wtfpl.net/txt/copying/
  */
-class MashinbazarCrawler extends BaseCrawler {
+class MashinbazarCrawler extends PhoneExtractorCrawler {
 
 	/**
 	 * URLs to crawl.
@@ -31,6 +34,18 @@ class MashinbazarCrawler extends BaseCrawler {
 	public $_url = 'http://www.mashinbazar.com/search/json/a:list';
 
 	/**
+	 * Array of URI regexes and associated POST data.
+	 *
+	 * @var array
+	 */
+	protected $_post = array(
+		'/^http:\/\/www\.mashinbazar\.com\/search\/json\/a:list$/' => array(
+			'iDisplayStart'  => 0,
+			'iDisplayLength' => 10,
+		),
+	);
+
+	/**
 	 * CSS selector.
 	 *
 	 * @var string
@@ -42,27 +57,93 @@ class MashinbazarCrawler extends BaseCrawler {
 	 *
 	 * @var string
 	 */
-	protected $_follow = '/\\"\\\/ads\\\/view\\\/id:\d+\\"/miu';
-	// protected $_follow = '/(http:\/\/(www\.)?mashinbazar.com\/search\/json\/)?$/miu';
-
-	/**
-	 * Regex(es) to match against request referer.
-	 *
-	 * This will let us manually maintain the crawl DEPTH,
-	 * since it's not currently supported by the underlying
-	 * PHPCrawler.
-	 *
-	 * Set to FALSE in order to disable the referer check.
-	 *
-	 * @var string
-	 */
-	//protected $_referer = '/(http:\/\/(www\.)?cartel.ir)?\/ajax.php\?search=0&page=[1-9]+/miu';
+	protected $_follow = '/INTENTIONAL WRONG REGEX; SEE: process_first()/';
 
 	// ------------------------------------------------------------------------
 
-	public function process()
+	/**
+	 * Processes the first crawled page.
+	 *
+	 * NOTE: We're reading URLs to follow from a JSON file. The address to this JSON endpoint
+	 * has been set as the starting URL of the adapter. The following method has wrapped the
+	 * logic of parsing the JSON and extracting the URLs to follow on the forthcoming requests.
+	 *
+	 * Because of this (manually extracting the URLs to crawl) we need to set a invalid pattern
+	 * in $_follow to avoid extra requests. So, the underlying crawler which respects the $_follow
+	 * would find no links to add to the LinkCache. Instead we're adding our desired links here...
+	 * manually.
+	 *
+	 * @return mixed
+	 */
+	public function process_first()
 	{
+		global $cli;
 
+		if ( ! empty($this->_content) AND $json = json_decode($this->_content))
+		{
+			// Parse JSON and findout the child URLs to crawl
+			if (empty($json->aaData))
+			{
+				$cli->err('"aaData" field is empty. No ads to crawl.')->eol();
+				return FALSE;
+			}
+
+			$count = 0;
+			foreach ($json->aaData as $ad)
+			{
+				// Seemingly wrapped in another array juss for fuckin fun!
+				$ad     = $ad[0];
+				$html   = str_get_html($ad);
+				$anchor = $html->find('a');
+
+				if (isset($anchor[0]) AND $anchor = $anchor[0]->getAttribute('href'))
+				{
+					$count++;
+					$anchor = 'http://www.mashinbazar.com' . $anchor;
+
+					// Push to the URL cache of the crawler
+					$this->_add_url($anchor);
+				}
+			}
+
+			$cli->info("Manually extracted and rewritten $count URLs to be crawled.")->eol();
+		}
+
+		// Invalid response
+		else
+		{
+			$cli->err('Invalid JSON response.')->eol();
+			return FALSE;
+		}
+	}
+
+	// ------------------------------------------------------------------------
+
+	/**
+	 * Configures the crawler to crawl new pages.
+	 *
+	 * @param  int $limit Crawler hit limit.
+	 *
+	 * @return void
+	 */
+	protected function _setup_new($limit = FALSE)
+	{
+		$limit OR $limit = 51;
+		$this->_post['/^http:\/\/www\.mashinbazar\.com\/search\/json\/a:list$/']['iDisplayLength'] = intval($limit) + 1;
+	}
+
+	// ------------------------------------------------------------------------
+
+	/**
+	 * Configures the crawler to crawl all possible pages.
+	 *
+	 * @param  int $limit Crawler hit limit.
+	 *
+	 * @return void
+	 */
+	protected function _setup_all($limit = FALSE)
+	{
+		$this->_post['/^http:\/\/www\.mashinbazar\.com\/search\/json\/a:list$/']['iDisplayLength'] = 440;
 	}
 
 	// ------------------------------------------------------------------------
